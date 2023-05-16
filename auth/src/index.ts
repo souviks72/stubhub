f390c3f9627387de0743289dba8f70960b4ac51e
souviks72 <souvikmandalvit2016@gmail.com>
2023-05-16 09:59:14 +0530

    adding error handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
+import "express-async-errors"; //for handling async errors without next() or try-catch
 import { json } from "body-parser";
 
-import { currentUserRouter } from "../routes/current-user";
-import { signinRouter } from "../routes/signin";
-import { signupRouter } from "../routes/signup";
-import { signoutRouter } from "../routes/signout";
-import { errorHandler } from "../middlewares/error-handler";
+import { currentUserRouter } from "./routes/current-user";
+import { signinRouter } from "./routes/signin";
+import { signupRouter } from "./routes/signup";
+import { signoutRouter } from "./routes/signout";
+import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 app.use(json());
@@ -14,6 +16,10 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signupRouter);
 app.use(signoutRouter);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
 
 app.use(errorHandler);
 
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,6 +1,9 @@
 import express, { Request, Response } from "express";
 import { body, validationResult } from "express-validator";
 
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
+
 const router = express.Router();
 
 router.post(
@@ -16,12 +19,12 @@ router.post(
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      throw new Error("Invalid email or password");
+      throw new RequestValidationError(errors.array());
     }
 
     const { email, password } = req.body;
     console.log("Creating an user...");
-    throw new Error("Eror connecting to databse");
+    throw new DatabaseConnectionError();
 
     res.send({ email, password });
   }
