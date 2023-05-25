b16b216689ad0c1fe18f201d94f89d014511ac15
souviks72 <souvikmandalvit2016@gmail.com>
2023-05-26 00:42:29 +0530

    completing auth logic

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,10 +1,10 @@
 import { CustomError } from "./custom-error";
 
 export class DatabaseConnectionError extends CustomError {
-  reason = "Error connecting to databse";
+  reason = "Error connecting to database";
   statusCode = 500;
   constructor() {
-    super("Error connecting to databse");
+    super("Error connecting to database");
 
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
   }
--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,27 @@
-import express from "express";
+import express, { Request, Response } from "express";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
-router.get("/api/users/currentuser", (req, res) => {
-  res.send("Hi there");
+router.get("/api/users/currentuser", (req: Request, res: Response) => {
+  if (!req.session?.jwt) {
+    return res.status(200).json({ currentUser: null });
+  }
+
+  try {
+    const payload = jwt.verify(req.session.jwt, process.env.JWT_KEY!);
+    return res.status(200).json({ currentUser: payload });
+  } catch (err) {
+    return res.status(200).json({ currentUser: null });
+  }
 });
 
 export { router as currentUserRouter };
+/*
+Our React front end wont be able to decode the JWT
+and extract user data. So when needed, it will query
+this endpoint and if the cookie  is not set, it means 
+user is not logged in. If cookie is present:
+- check if JWT is valid
+- extract info and send to front end
+*/
